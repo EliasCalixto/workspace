41389bdf92a8ef7eecd8cb0d472eba618418bcdf
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2024-02-09 16:32:02 -0500

    rgg

--- tools/moneyStatus.py
+++ tools/moneyStatus.py
@@ -6,7 +6,7 @@ try:
     dataFrame = pd.read_excel("C:\\Users\\elias\\OneDrive\\Documentos\\Main.xlsx", sheet_name='data')
     allData = np.array(dataFrame)
 except:
-    dataFrame = pd.read_excel("/Users/eliascalixto/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx", sheet_name='data')
+    dataFrame = pd.read_excel("/Users/darkesthj/Library/CloudStorage/OneDrive-Personal/Documentos/Main.xlsx", sheet_name='data')
     allData = np.array(dataFrame)
 
 def getCurrentMonth():
