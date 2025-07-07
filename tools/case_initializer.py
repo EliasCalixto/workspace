05f83659f2978bd5605adec4ec561cf4749edd72
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2025-07-07 01:34:13 -0500

    07/07

--- tools/case_initializer.py
+++ tools/case_initializer.py
@@ -3,18 +3,15 @@ import pandas as pd
 
 
 def init_case(case_number: int, data_frame: list, file_path: str) -> None:
-    """
-
-    :rtype: object
-    """
     try:
         os.mkdir(f'APM files/{case_number}')
         print('Directory created successfully.')
     except:
         pass
+    
+    df_file = pd.DataFrame(data_frame)
 
     #Create Excel
-    df_file = pd.DataFrame(data_frame)
     file_path = f'APM files/{case_number}/{case_number} {file_path}'
     
     df_file.to_excel(file_path, index=False)
