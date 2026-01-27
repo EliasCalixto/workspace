d3000c4651df3a2d17478ed3aa7209dc3a270f28
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2026-01-27 13:06:40 -0500

    01/27

---
+++ expedia/macro_mac/v1.2/create_dmg 13.48.36.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+APP_NAME="CoreOTA Macro"
+VERSION="v1.2"
+DMG_NAME="${APP_NAME}_${VERSION}.dmg"
+VOL_NAME="${APP_NAME} Installer"
+
+# Eliminar versiones anteriores
+rm -f "$DMG_NAME"
+rm -rf ./dmg_temp
+
+# Crear carpeta temporal
+mkdir dmg_temp
+cp -R "dist/${APP_NAME}.app" dmg_temp/
+
+# Crear el .dmg
+hdiutil create "$DMG_NAME" \
+  -volname "$VOL_NAME" \
+  -srcfolder "dmg_temp" \
+  -ov -format UDZO
+
+# Limpiar
+rm -rf dmg_temp
